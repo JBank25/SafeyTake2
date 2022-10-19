d5b453272fe125c76147949c3600066be5ec6df7
JayBank22 <70767462+JayBank22@users.noreply.github.com>
2022-10-18 20:12:54 -0500

    Initial Commit

---
+++ Safety-22/Safety_22App.swift
@@ -0,0 +1,17 @@
+//
+//  Safety_22App.swift
+//  Safety-22
+//
+//  Created by Joshua Urbank on 10/18/22.
+//
+
+import SwiftUI
+
+@main
+struct Safety_22App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
